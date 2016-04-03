ab8a84ea3c7ec412f7afe1168cc483a4bbc5042b
Anastasiya Lobova <anastasiya.k91@gmail.com>
2016-04-04 00:16:24 +0300

    Настроила автоматизацию

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,16 @@ var mixins = require("postcss-mixins");
 var minmax = require("postcss-media-minmax");
 var mqpacker = require("css-mqpacker");
 var server = require("browser-sync");
-var mystyle = "postcss/style.css";
+var minify = require("gulp-csso");
+var rename = require("gulp-rename");
+var imagemin = require("gulp-imagemin");
+var svgmin = require("gulp-svgmin");
+var clean = require("gulp-clean");
+var htmlmin = require("gulp-htmlmin");
+var concat = require("gulp-concat");
+var uglify = require("gulp-uglify");
+var runSequence = require("gulp-run-sequence");
+var mystyle = "source/postcss/style.css";
 var pluginList = [
   importcss(),
   mixins(),
@@ -28,12 +37,106 @@ var pluginList = [
   ]})
 ]
 
+// Clean
+gulp.task("clean", function () {
+  return gulp.src("build/*", {read: false})
+    .pipe(clean())
+});
+
+// Copy
+gulp.task("copy", function() {
+  gulp.src("source/img/**")
+    .pipe(gulp.dest("build/img"))
+  gulp.src("source/fonts/**/*.{woff,woff2}")
+    .pipe(gulp.dest("build/fonts"))
+  gulp.src("source/*.html")
+    .pipe(gulp.dest("build"))
+  gulp.src("source/js/vendor/*.js")
+    .pipe(gulp.dest("build/js/vendor"))
+});
+
+// Copy css plugins
+gulp.task("copyStylePlugins", function() {
+  gulp.src("node_modules/flickity/dist/flickity.min.css")
+    .pipe(gulp.dest("source/postcss/vendor"))
+  gulp.src("node_modules/normalize.css/normalize.css")
+    .pipe(gulp.dest("source/postcss/vendor"))
+});
+
+// Combine styles
 gulp.task("style", function() {
   gulp.src(mystyle)
     .pipe(plumber())
     .pipe(postcss(pluginList))
-    .pipe(gulp.dest("css"))
+    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
+    .pipe(minify())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(server.reload({stream: true}));
+});
+
+// Combine script
+gulp.task("script", function() {
+  return gulp.src(["source/js/*.js"])
+    .pipe(plumber())
+    .pipe(concat("script.js"))
+    .pipe(gulp.dest("source/js"))
+    .pipe(gulp.dest("build/js"))
+    .pipe(uglify())
+    .pipe(rename("script.min.js"))
+    .pipe(gulp.dest("build/js"));
+})
+
+// Copy plugins
+gulp.task("copyPlugins", function() {
+  gulp.src("node_modules/flickity/dist/flickity.pkgd.min.js")
+    .pipe(gulp.dest("source/js/vendor"))
+  gulp.src("node_modules/picturefill/dist/picturefill.min.js")
+    .pipe(gulp.dest("source/js/vendor"))
+});
+
+// Image optimization
+gulp.task("images", function() {
+  return gulp.src("build/img/**/*.{png,jpg,gif}")
+  .pipe(imagemin({
+    optimizationLevel: 3,
+    progressive: true
+  }))
+  .pipe(gulp.dest("build/img"));
+});
+
+// Svg minify
+gulp.task("svg", function () {
+  return gulp.src("build/img/**/*.svg")
+    .pipe(svgmin())
+    .pipe(gulp.dest("build/img"));
+});
+
+// Remove comment html
+gulp.task("htmlmin", function() {
+  return gulp.src("build/*.html")
+    .pipe(htmlmin({
+      removeComments: true,
+      collapseWhitespace: true
+    }))
+    .pipe(rename({
+      suffix: ".min"
+    }))
+    .pipe(gulp.dest("build"));
+});
+
+// Build
+gulp.task("build", function() {
+  runSequence(
+    "clean",
+    "copy",
+    "style",
+    "images",
+    "svg",
+    "htmlmin",
+    "script"
+  );
 });
 
 gulp.task("serve", ["style"], function() {
@@ -44,6 +147,6 @@ gulp.task("serve", ["style"], function() {
     ui: false
   });
 
-  gulp.watch("postcss/**/*.css", ["style"]);
-  gulp.watch("*.html").on("change", server.reload);
+  gulp.watch("source/postcss/**/*.css", ["style"]);
+  gulp.watch("source/*.html").on("change", server.reload);
 });
