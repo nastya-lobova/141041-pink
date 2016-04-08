42efb36c4195d966e858080edc6e1428a4a2d0c1
Анастасия Лобова <anastasiya.k91@gmail.com>
2016-04-08 17:54:02 +0300

    Исправила скрипт для меню

--- source/js/main-menu.js
+++ source/js/main-menu.js
@@ -2,26 +2,25 @@
   var header = document.querySelector(".page-header");
   var menu = header.querySelector(".page-header__menu-inner");
   var button = header.querySelector(".main-menu__btn");
-  var iconCross = header.querySelector(".main-menu__btn-cross");
-  var iconHam = header.querySelector(".main-menu__btn-hamburger");
   var list = header.querySelector(".main-menu__list");
 
-  if( menu != undefined || button != undefined || iconCross != undefined || iconHam != undefined || list != undefined) {
+  console.log(button);
+
+  if( menu != undefined || list != undefined) {
+
     if (header.classList.contains("page-header--main-open")) {
       header.classList.remove("page-header--main-open");
     }
+    
     header.classList.remove("page-header--open");
     menu.classList.remove("page-header__menu-inner--open");
-    iconCross.classList.remove("main-menu__btn-cross--show");
-    iconHam.classList.remove("main-menu__btn-hamburger--hide");
     list.classList.add("main-menu__list--open");
     list.classList.add("main-menu__list--hide");
 
     button.addEventListener("click", function(event){
+
       event.preventDefault();
       menu.classList.toggle("page-header__menu-inner--open");
-      iconCross.classList.toggle("main-menu__btn-cross--show");
-      iconHam.classList.toggle("main-menu__btn-hamburger--hide");
       list.classList.toggle("main-menu__list--hide");
     });
   }
